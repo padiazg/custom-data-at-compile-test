3fc0c356295f442c4f272c78604c0f5669bedcc9
padiazg <padiazg@gmail.com>
2022-07-22 21:31:27 -0400

    wip

--- main.go
+++ main.go
@@ -6,6 +6,6 @@ func main() {
 	fmt.Printf("Build Time: %s\n", GetBuildTime())
 	fmt.Printf("Commit Hash: %s\n", GetCommitHash())
 	fmt.Printf("Version: %s\n", GetVersion())
-	fmt.Printf("Formatted: %s\t SHA: %s\n", GetVersion(), GetCommitHash())
+	fmt.Printf("Formatted: %s\t SHA: %s\t Build date: %s \n", GetVersion(), GetCommitHash(), GetBuildTime())
 
 }
